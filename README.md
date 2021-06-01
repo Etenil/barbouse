6c6b31be12700a45c3e2afa73b7b0a60a7b61734
Guillaume Pasquet <guillaume@bitimplosion.com>
2021-06-01 12:04:02 +0100

    Working requests.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# barbouse
-Postman is for wimps
+# BARBOUSE
+
+Postman for bearded API users.
+
+Barbouse is an API exploration/testing utility that relies on a simple
+file format and leverages existing operating-system mechanics.
+
+## Quick start
+
+Making a request is a simple as writing the following file, let's call it `request.txt`:
+
+```
+#GET^http://myapi.com/some/resource
+```
+
+Run with:
+
+```
+barbouse request.txt
+```
+
+## File format
+
+The barbouse file format is as follows:
+
+```
+#METHOD^URL
+#HEADER: HEADER_VALUE
+#HEADER: HEADER_VALUE
+#...
+#|<JQ_FILTER>
+
+PAYLOAD
+```
+
+Headers, the JQ filter and the payload are optional. The empty line above the
+payload is significant and must be present.
+
+Refer to the [JQ documentation](https://stedolan.github.io/jq/manual/) for filters.
+
+
+## Running requests
+
+Barbouse can run multiple request files given on the command line:
+
+```
+barbouse <file1> <file2>
+```
--- barbouse/barbouse.py
+++ barbouse/barbouse.py
@@ -1,43 +1,84 @@
 import json
-from pygments import highlight
-from pygments.lexers import JsonLexer
-from pygments.formatters import TerminalFormatter
-import sys
 import os
-import requests as r
+import sys
+
 import jq
-import logging
+import requests as r
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import JsonLexer
 
-log = logging.getLogger("barbouse")
+from pprint import pprint
 
-class Command:
-    def __init__(self, cmd):
-        command = cmd.split("#")
-        self.method = command[0]
-        self.url = command[1]
+
+class ReqFile:
+    def _populate(self, subject):
         for varname in os.environ:
             placeholder = "{%s}" % (varname,)
-            self.url = self.url.replace(placeholder, os.environ.get(varname))
+            subject = subject.replace(placeholder, os.environ.get(varname))
+        return subject
 
-        print(self.url)
+    def __init__(self):
+        self.method = "GET"
+        self.url = None
+        self.headers = {}
         self.filtr = None
-        if len(command) > 2:
-            self.filtr = command[2]
+
+    @classmethod
+    def load(klass, filename):
+        req = klass()
+        with open(filename, "r") as f:
+            line = f.readline()
+            if line[0] != "#":
+                raise ValueError()
+
+            req.method, req.url = line[1:].split("^")
+            req.url = req._populate(req.url.strip())
+
+            # Headers and filter
+            line = f.readline()
+            while line[0] == "#":
+                if line[1] == "|":
+                    req.filtr = jq.compile(req._populate(line[2:].strip()))
+                elif ":" in line:
+                    key, val = line[1:].split(":")
+                    req.headers[key] = req._populate(val.strip())
+                line = f.readline()
+            req.payload = f.read()
+            if req.payload.strip() == "":
+                req.payload = None
+        return req
+
+    def execute(self):
+        print(f"{self.method} {self.url}")
+        return r.request(self.method, self.url, headers=self.headers, data=self.payload)
+
 
 def main():
-    filename = sys.argv[1]
-    with open(filename, 'r') as f:
-        cmd = Command(f.readline())
-        payload = f.read()
+    for filename in sys.argv[1:]:
+        req = ReqFile.load(filename)
+        resp = req.execute()
 
-    resp = r.request(cmd.method, cmd.url)
-    result = resp.json()
-    if cmd.filtr:
-        result = jq.compile(cmd.filtr).input(resp.json()).all()
+        print(f"{resp.status_code} {resp.reason}")
+        for hdr, val in resp.headers.items():
+            print(f"{hdr}: {val}")
 
-    print(highlight(json.dumps(result, indent=4, sort_keys=True), JsonLexer(), TerminalFormatter()))
-    
-    
+        print(" ")
+
+        result = resp.json()
+        if req.filtr:
+            result = req.filtr.input(result).all()
+        if len(result) == 1:
+            result = result[0]
+
+        print(
+            highlight(
+                json.dumps(result, indent=4, sort_keys=True),
+                JsonLexer(),
+                TerminalFormatter(),
+            )
+        )
+
 
 if __name__ == "__main__":
     main()
